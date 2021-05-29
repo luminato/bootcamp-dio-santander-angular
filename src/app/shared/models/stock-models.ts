8c1405ad6a6461ba884aee123166925afd645811
fabricio <luminato.silva@gmail.com>
2021-05-29 19:49:33 -0300

    primeira versão do frontend

---
+++ src/app/shared/models/stock-models.ts
@@ -0,0 +1,12 @@
+class Stock{
+  id: number;
+  name: string;
+  price: number;
+  minimum: number;
+  exclusiveMinimun: boolean;
+  variation: number;
+  date: string;
+}
+
+export default Stock;
+
